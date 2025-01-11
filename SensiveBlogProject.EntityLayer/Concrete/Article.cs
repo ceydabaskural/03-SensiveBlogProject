0761aa54e19d6b0325a04ef753a9de39b1171260
Ceyda Başkural <ceydabaskural8@gmail.com>
2025-01-11 15:19:42 +0300

    #24.10.2024 - entity layer created and 4 packages which are about entity framework core added to project

---
+++ SensiveBlogProject.EntityLayer/Concrete/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveBlogProject.EntityLayer.Concrete
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        public string ArticleName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } //bunu yazmamızın amacı ilişkiyi kurmak için ve Article sınıfı üzerinden Category sınıfına ulaşmak için bunu kullanırız
+    }
+}
